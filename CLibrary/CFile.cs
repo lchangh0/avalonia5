745fce1ee56f2a7c850ceb4d955fdd93ebc7bd79
lchangh0 <lchangh0@hanmail.net>
2025-07-06 22:08:25 +0900

    파일 트리 추가

--- CLibrary/CFile.cs
+++ CLibrary/CFile.cs
@@ -46,4 +46,84 @@ public static class CFile
     {
         File.WriteAllText(filePath, content);
     }
+
+
+
+    public class CFileNode
+    {
+        public enum ENodeType { Folder, File };
+
+        public ENodeType NodeType { get; set; }
+        public string Name { get; set; } = "";
+        public string Path { get; set; } = "";
+        public List<CFileNode> Children { get; set; } = new List<CFileNode>();
+
+    }
+
+    public static bool GetFileNodes(string rootPath,
+        out CFileNode nodeRoot)
+    {
+        nodeRoot = new CFileNode();
+
+        if (!Directory.Exists(rootPath))
+            return false;
+
+        nodeRoot.NodeType = CFileNode.ENodeType.Folder;
+        nodeRoot.Path = rootPath;
+        nodeRoot.Name = Path.GetFileName(rootPath) ?? "";
+
+        GetFileNodesSub(nodeRoot);
+
+        return true;
+    }
+
+    static bool GetFileNodesSub(CFileNode nodeParent)
+    {
+        try
+        {
+            string[] dirs = Directory.GetDirectories(nodeParent.Path);
+            foreach (var dir in dirs)
+            {
+                DirectoryInfo di = new DirectoryInfo(dir);
+                if ((di.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                    continue;
+
+                CFileNode nodeDir = new CFileNode();
+                nodeDir.NodeType = CFileNode.ENodeType.Folder;
+                nodeDir.Path = dir;
+                nodeDir.Name = Path.GetFileName(dir);
+                nodeParent.Children.Add(nodeDir);
+                //Log(nodeDir.Path);
+
+                GetFileNodesSub(nodeDir);
+            }
+
+            string[] files = Directory.GetFiles(nodeParent.Path);
+            foreach (var file in files)
+            {
+                CFileNode nodeFile = new CFileNode();
+                nodeFile.NodeType = CFileNode.ENodeType.File;
+                nodeFile.Path = file;
+                nodeFile.Name = Path.GetFileName(file);
+                nodeParent.Children.Add(nodeFile);
+                //Log(nodeFile.Path);
+            }
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    static CLogger? _logger = null;
+    static void Log(string msg, ELogLevel logLevel = ELogLevel.Info)
+    {
+        if (_logger == null)
+            _logger = new CLogger("CFile");
+        _logger.Log(msg, logLevel);
+    }
+
+
 }
--- Main/MainWindow.axaml.cs
+++ Main/MainWindow.axaml.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.VisualTree;
 using CLibray;
 using Main;
 
@@ -14,12 +18,57 @@ public partial class MainWindow : Window
     {
         _logger = new CLibray.CLogger("MainWindow");
         InitializeComponent();
+
+        treeFile.SelectionChanged += treeFile_SelectionChanged;
     }
 
-    async void btnFile_Click(object sender, RoutedEventArgs args)
+    void btnFile_Click(object sender, RoutedEventArgs args)
     {
-        await OpenFile();
+        CFile.GetFileNodes("/home/user", out CFile.CFileNode nodeRoot);
+        PopulateFileTree(nodeRoot);
     }
+
+    void PopulateFileTree(CFile.CFileNode nodeRoot)
+    {
+        treeFile.Items.Clear();
+        PopulateFileTreeSub(treeFile.Items, nodeRoot);
+    }
+
+    void PopulateFileTreeSub(ItemCollection parentItems, CFile.CFileNode nodeParent)
+    {
+        foreach (var node in nodeParent.Children)
+        {
+            TreeViewItem tvi = new TreeViewItem();
+            tvi.Header = node.Name;
+            tvi.Tag = node;
+            parentItems.Add(tvi);
+
+            if (node.NodeType == CFile.CFileNode.ENodeType.Folder)
+            {
+                tvi.Background = new SolidColorBrush(Colors.LightGreen);
+                PopulateFileTreeSub(tvi.Items, node);
+            }
+        }
+
+    }
+    void treeFile_SelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (e.AddedItems[0] is TreeViewItem selectedItem)
+        {
+            txtFile.Text = selectedItem.Header?.ToString() ?? string.Empty;
+            _logger.Log($"treeFile_SelectedItemChanged() // Selected={txtFile.Text}");
+
+            CFile.CFileNode? node = selectedItem.Tag as CFile.CFileNode;
+            if (node != null)
+            {
+                if (node.NodeType == CFile.CFileNode.ENodeType.File)
+                {
+                    txtContent.Text = CFile.ReadAllText(node.Path);
+                }
+            }
+        }
+    }
+
 
     async void fileOpen_Click(object sender, RoutedEventArgs args)
     {
